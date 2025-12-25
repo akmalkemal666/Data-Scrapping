6aa18a3c69f1c4ea2aa1b2c0b7d2ebdcf3d8ae83
akmalkemal666 <akmalhafid70@gmail.com>
2025-12-25 08:50:45 +0700

    Add files via upload

---
+++ pt 11/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Server Flask jalan"
+
+@app.route('/login', methods=['GET'])
+def login():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    if username == 'admin' and password == '123':
+        return "Login sukses bolo"
+    else:
+        return "Login gagal bolo"
+
+if __name__ == "__main__":
+    app.run(debug=True)
